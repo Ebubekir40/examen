cb1a07d7a7155e9f1b917d727a17b438d2e140d5
Ebubekir40 <94608924+Ebubekir40@users.noreply.github.com>
2024-05-07 16:05:27 +0200

    navbar voor na het inloggen

---
+++ hoofd1.php
@@ -0,0 +1,86 @@
+<?php
+session_start();
+?>
+
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <style>
+            body {
+                padding-top: 150px;
+            }
+
+            .navbar {
+                background-color: white;
+                box-shadow: 0 4px 2px -2px gray;
+            }
+
+            .navbar-brand img {
+                max-height: 125px;
+            }
+
+            .navbar-nav {
+                justify-content: space-between;
+            }
+
+            .navbar-nav.ml-auto {
+                margin-right: 0;
+            }
+
+            .nav-item {
+                margin-right: 45px;
+            }
+
+            .navbar-nav .nav-link {
+                color: black !important;
+                font-size: 20px;
+                font-weight: bold;
+            }
+
+
+        </style>
+    </head>
+
+    <body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top">
+            <a class="navbar-brand" href="index.php"><img src="pochonlogo" alt="Logo"></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="alleproducten.php">Producten</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="createproduct1.php">Product toevoegen</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="beheerdersdashboard.php">laatst geüpdatet </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="beheerdersprofiel.php">Beheerdersprofiel</a>
+                    </li>
+                    <li class="nav-item">
+                        <?php
+                        if (isset($_SESSION['beheerderid'])) {
+                            echo '<a class="nav-link" href="uitloggen.php">Uitloggen</a>';
+                        }
+                        ?>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    </body>
+
+    </html>
+
