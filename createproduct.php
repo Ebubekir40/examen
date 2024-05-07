479e25dcf2986dc7fa9b468822b75ba030ea7e2b
Ebubekir40 <94608924+Ebubekir40@users.noreply.github.com>
2024-05-07 13:18:41 +0200

    Create product

---
+++ createproduct.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+require_once 'producten.php';
+
+
+if ($_FILES['afbeelding']['error'] == 4) {
+    // Er is geen afbeelding geüpload
+    $afbeelding = null;
+} elseif ($_FILES['afbeelding']['error'] !== UPLOAD_ERR_OK) {
+    echo "Er is een fout opgetreden bij het uploaden van de afbeelding: " . $_FILES['afbeelding']['error'];
+    exit();
+} else {
+    $uploadDirectory = 'uploads/';
+    $uploadedFile = $uploadDirectory . basename($_FILES['afbeelding']['name']);
+
+    if (move_uploaded_file($_FILES['afbeelding']['tmp_name'], $uploadedFile)) {
+        $afbeelding = $uploadedFile;
+    } else {
+        echo "Er is een fout opgetreden bij het uploaden van de afbeelding.";
+        exit();
+    }
+}
+
+// Controleert of 'aantal' een numerieke waarde is
+$aantal = $_POST['aantal'];
+if (!is_numeric($aantal)) {
+    die("Aantal moet een numerieke waarde zijn.");
+}
+
+$productnaam = $_POST['productnaam'];
+$stellingsnummer = $_POST['stellingsnummer'];
+
+$producten = new Producten();
+$productinfo = $producten->createProduct($productnaam, $afbeelding, $aantal, $stellingsnummer);
+
+$message = "Product succesvol toegevoegd. Product ID: {$productinfo['productid']}";
+$_SESSION['success_message'] = $message;
+
+header("Location: createproduct1.php");
+exit();
+?>
---
+++ createproduct1.php
@@ -0,0 +1,63 @@
+<?php
+require_once 'producten.php';
+require_once 'hoofd1.php';
+
+if (!isset($_SESSION['beheerderid'])) {
+    header("Location: beheerderlogin.php");
+    exit();
+}
+
+
+$producten = new Producten();
+// Alle gebruikte stellingsnummers ophalen
+$gebruikteStellingen = $producten->getGebruikteStellingen();
+$beschikbareStellingen = array_diff(range(1, 25), $gebruikteStellingen);
+?>
+
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <title>Product Toevoegen</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+          rel="stylesheet" integrity="sha384-rSWY5G/6S2X8K2V5/6S2D/jJb8tcPfVugWCPtgaU8BwhBIJjNqcyzy3W6F5wEWTl"
+          crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+    <h2 class="mt-3">Product Toevoegen</h2>
+    <form action="createproduct.php" method="post" enctype="multipart/form-data">
+
+        <div class="mb-3">
+            <label for="productnaam" class="form-label">Productnaam:</label>
+            <input type="text" class="form-control" id="productnaam" name="productnaam" required>
+        </div>
+
+        <div class="mb-3">
+            <label for="aantal" class="form-label">Aantal:</label>
+            <input type="number" class="form-control" id="aantal" name="aantal" required>
+        </div>
+        <div class="mb-3">
+            <label for="stellingsnummer" class="form-label">Stellingsnummer:</label>
+            <select class="form-select" id="stellingsnummer" name="stellingsnummer" required>
+                <option value="" selected disabled>Kies een stellingsnummer</option>
+                <?php foreach ($beschikbareStellingen as $stellingnummer) : ?>
+                    <option value="<?php echo $stellingnummer; ?>"><?php echo $stellingnummer; ?></option>
+                <?php endforeach; ?>
+            </select>
+        </div>
+
+        <div class="mb-3">
+            <label for="afbeelding" class="form-label">Afbeelding uploaden:</label>
+            <input type="file" class="form-control" id="afbeelding" name="afbeelding" accept="image/*">
+        </div>
+
+        <input type="submit" class="btn btn" value="Product Toevoegen" style="background-color: black; color: white;">
+    </form>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
+</body>
+</html>
